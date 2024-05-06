d9d6d20b626aa1040e1d9ea46a8cfb996953a0cf
Jake Loew <jake@madebychalk.com>
2024-05-06 11:08:27 -0600

    Upgrade to SDK v1.0

--- complete-application/src/app/home-page/home-page.component.ts
+++ complete-application/src/app/home-page/home-page.component.ts
@@ -1,16 +1,15 @@
-import {Component, inject} from '@angular/core';
-import {FusionAuthService} from "@fusionauth/angular-sdk";
-import {CommonModule} from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { FusionAuthService } from '@fusionauth/angular-sdk';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
-
   private fusionAuthService = inject(FusionAuthService);
 
   login() {
